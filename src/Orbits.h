2613138f210fa9405408a2dbf8eb7f4f2500fb07
Joseph Larralde <joseph.larralde@gmail.com>
2023-10-02 12:00:53 +0200

    cleaned a bit

--- src/Orbits.h
+++ src/Orbits.h
@@ -2,11 +2,9 @@
 #define ORBITS_ORBITS_H
 
 #include <map>
+#include <cmath>
 
-// technique to allow definition of template class methods in cpp file
-// and still be able to avoid "Symbol not found" errors
-// the alternative is to drop the cpp file and put all the code in the header
-#include "./core/Tree.cpp"
+#include "./core/Tree.h"
 
 namespace Orbits {
 
@@ -23,30 +21,34 @@ public:
 // private:
   typedef Mass<dimension> Mass;
   typedef std::shared_ptr<Mass> MassPtr;
-  //typedef std::shared_ptr<Tree<dimension>> TreePtr;
-  
+
 private:
   std::vector<MassPtr> particles; // the particles
   std::unique_ptr<Tree<dimension>> tree; // the tree to compute closest particles
-  // Tree<dimension> tree;
   std::map<const char*, std::shared_ptr<Rule>> rules; // the navigation rules to apply
 
 public:
-  Orbits() {
-    tree.reset(new Tree<dimension>());
+  Orbits(std::size_t s) : tree(nullptr) {
+    setSize(s);
   }
 
   void setSize(std::size_t s) {
     tree.reset(new Tree<dimension>());
-    // particles.clear();
     particles.resize(s);
+
     for (std::size_t i = 0; i < s; ++i) {
-      auto p = std::make_shared<Mass>();
-      particles[i] = p;
+      particles[i] = std::make_shared<Mass>();
     }
   }
 
-  void setBounds() {}
+  std::size_t getSize() {
+    return particles.size();
+  }
+
+  // void setBounds() {}
+  // => bounds are deduced from all particles' positions, and clipping is
+  // performed internally by Tree according to DEFAULT_MIN_DIMENSION_BOUND
+  // and DEFAULT_MAX_DIMENSION_BOUND
   
   // set particles basic physical parameters :
   void setM(float m) { for (auto p : tree.particles) p->setM(m); } // mass
@@ -58,8 +60,9 @@ public:
   }
 
   void setParameter(const char* ruleName, const char* parameterName, const std::vector<float>& values) {
-    
-    // if (std::strcmp(parameterName, "stuff") == 0) { rules[ruleName]->setParameter(parameterValue); }
+    // if (std::strcmp(parameterName, "stuff") == 0) {
+    //   rules[ruleName]->setParameter(parameterValue);
+    // }
     rules[ruleName]->setParameter(parameterName, values);
   }
   
@@ -69,14 +72,12 @@ public:
 
     for (auto& [ ruleName, rule ] : rules) {
       rule->process(tree.get());
-      //rule->process(&tree);
     }
   }
 
   std::vector<std::vector<float>>
   getPositions() {
     std::vector<std::vector<float>> res;
-    // for (auto& p : tree->particles) { // below should be equivalent
     for (auto& p : particles) {
       res.push_back(p.getPosition());
     }
@@ -86,11 +87,24 @@ public:
   std::vector<std::pair<std::vector<float>, std::vector<float>>>
   getPositionsAndSpeeds() {
     std::vector<std::pair<std::vector<float>, std::vector<float>>> res;
-    // for (auto& p : tree->particles) { // below should be equivalent
     for (auto& p : particles) {
       res.push_back({ p.getPosition(), p.getSpeed() });
     }
     return res;
+  }
+
+private:
+  void setRandomParticlePositions() {
+    std::srand(std::time(nullptr));
+    std::vector<float> position(3, 0.f);
+
+    for (std::size_t i = 0; i < particles.size(); ++i) {
+      particles[i] = std::make_shared<Mass>();
+      for (std::size_t d = 0; d < dimension; ++d) {
+        position[d] = static_cast<float>(std::rand()) / RAND_MAX;
+      }
+      particles[i]->setPosition(position);
+    }
   }
 };
 
