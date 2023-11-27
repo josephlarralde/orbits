7c7ac97a8b991e82e4ecbbb31b9af79eb3dd07a5
Joseph Larralde <joseph.larralde@gmail.com>
2023-11-27 11:00:14 +0100

    minor changes + uniform random speed generator

--- src/Orbits.h
+++ src/Orbits.h
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <cmath>
+#include <random>
 #include <chrono>
 
 #include "./core/Tree.h"
@@ -15,7 +16,8 @@ public:
   typedef Mass<dimension> Mass;
   typedef std::shared_ptr<Mass> MassPtr;
 
-  // simple definition of Rule class
+  // simple definition of Rule class ///////////////////////////////////////////
+
   class Rule {
   protected:
     std::map<const char*, std::function<void(const std::vector<float>&)>> setters;
@@ -41,16 +43,16 @@ public:
 
     // clunky API until I figure out how to write a Mass iterator on the Tree to
     // get rid of the reference to particles here :
-    virtual void processParticles(Tree<dimension>* t, std::vector<MassPtr>& v, float dt) {}
-    // virtual void processParticlesTree(Tree<dimension>* t) {}
-    // virtual void processParticlesVector(std::vector<MassPtr>& v) {}
+    virtual void processParticles(Tree<dimension>* t, std::vector<MassPtr>& v, float dt) = 0;
   };
 
 private:
   std::chrono::time_point<std::chrono::steady_clock> lastTickDate;
   std::vector<MassPtr> particles; // the particles
   std::unique_ptr<Tree<dimension>> tree; // the tree to compute closest particles
-  std::map<const char*, std::shared_ptr<Rule>> rules; // the navigation rules to apply
+
+  std::map<const char*, std::shared_ptr<Rule>> rulesById; // the navigation rules to apply
+  std::vector<std::shared_ptr<Rule>> rules;
 
 public:
   Orbits(std::size_t s) : tree(nullptr) {
@@ -77,12 +79,15 @@ public:
   
   // set particles basic physical parameters :
   void setM(float m) { for (auto p : particles) p->setM(m); } // mass
+
   // damping doesn't make sense for a mass alone !!!
   // void setD(float d) { for (auto p : particles) p->setD(d); } // damping
+
   void setF(float f) { for (auto p : particles) p->setF(f); } // friction
 
   void addRule(const char* ruleName, std::shared_ptr<Rule> rule) {
-    rules[ruleName] = rule;
+    rulesById[ruleName] = rule;
+    rules.push_back(rule);
   }
 
   void setParameter(const char* ruleName,
@@ -108,9 +113,8 @@ public:
     tree->setup(particles);
 
     // apply rules
-    // todo : add optional dt argument to process / update methods
-    for (auto& [ ruleName, rule ] : rules) {
-      rule->processParticles(tree.get(), particles, dt);
+    for (std::size_t i = 0; i < rules.size(); ++i) {
+      rules[i]->processParticles(tree.get(), particles, dt);
     }
 
     for (auto m : particles) m->update(dt);
@@ -146,6 +150,34 @@ private:
       }
       particles[i]->setPosition(position);
     }
+  }
+
+  // todo implement this in an efficient way
+  void setRandomParticleSpeeds() {
+
+    // create a set of random points uniformly distributed on the
+    // (dimension-1)-sphere (or n-ball).
+    // see https://en.wikipedia.org/wiki/N-sphere#Uniformly_at_random_on_the_(n_%E2%88%92_1)-sphere
+    // and https://en.wikipedia.org/wiki/N-sphere#Uniformly_at_random_within_the_n-ball
+    // we use normal deviates computed from std::normal_distribution for each
+    // dimension, then we normalized the obtain vectors, and voilà !
+    // (actually we use a single normal deviate for all dimension, but it should
+    // behave pretty much the same).
+
+    std::vector<float> point(dimension);
+    std::random_device seed_generator;
+    std::default_random_engine generator(seed_generator);
+    std::normal_distribution<float> distribution{0.5, 0.2};
+
+    for (std::size_t i = 0; i < particles.size(); ++i) {
+      for (std::size_t d = 0; d < dimension; ++d) {
+        point[d] = distribution(generator);
+      }
+      particles[i]->setSpeed(point);
+    }
+    std::vector<float> dist(particles.size());
+
+
   }
 };
 
